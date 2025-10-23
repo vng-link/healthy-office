0ae9dba5818247923ce200d1ba71fe838a36cef9
vadim.vladyka <vadim.vladyka@yandex.ru>
2025-10-23 17:55:39 +0300

    working

--- source/js/modules/init-header-scroll.js
+++ source/js/modules/init-header-scroll.js
@@ -1,6 +1,7 @@
 const initHeaderScroll = () => {
   const pageWrapper = document.querySelector('[data-page-wrapper]');  
   const header = document.querySelector('[data-header]');
+  const breakpoint = window.matchMedia(`(max-width:1200px)`);
 
   if (!pageWrapper || !header) {
     return;
@@ -12,11 +13,18 @@ const initHeaderScroll = () => {
   function trackScroll() {
 
     const pageWrapperCoordY = document.querySelector('[data-page-wrapper]').getBoundingClientRect().y;
-
-    if (pageWrapperCoordY > 38) {
-        header.classList.remove('is-active'); 
+    if (breakpoint.matches) {
+      if (pageWrapperCoordY > 50) {
+          header.classList.remove('is-active'); 
+      }else {
+        header.classList.add('is-active');
+      }
     }else {
-       header.classList.add('is-active');
+      if (pageWrapperCoordY > 38) {
+          header.classList.remove('is-active'); 
+      }else {
+        header.classList.add('is-active');
+      }
     }
 
   }
